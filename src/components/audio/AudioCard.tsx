e33c0263c2045e20fc72ee3dd94d1e3092d1b23d
kapehh <mail@kapehh.net>
2024-07-29 01:20:18 +0300

    fix images

--- src/components/audio/AudioCard.tsx
+++ src/components/audio/AudioCard.tsx
@@ -1,55 +1,12 @@
-import play from '../../assets/play.svg'
-import pause from '../../assets/pause.svg'
-import next from '../../assets/next.svg'
-import previous from '../../assets/previous.svg'
-import muted from '../../assets/muted.svg'
-import volume from '../../assets/volume.svg'
-import volume_small from '../../assets/volume-small.svg'
-import volume_loud from '../../assets/volume-loud.svg'
+import Pause from '../images/pause.tsx'
+import Play from '../images/play.tsx'
+import './AudioCard.css'
 
-export default function () {
+export default function() {
     return (
         <div>
-            <img
-                width={35}
-                height={35}
-                src={volume}
-            />
-            <img
-                width={35}
-                height={35}
-                src={volume_small}
-            />
-            <img
-                width={35}
-                height={35}
-                src={volume_loud}
-            />
-            <img
-                width={35}
-                height={35}
-                src={muted}
-            />
-            <img
-                width={35}
-                height={35}
-                src={previous}
-            />
-            <img
-                width={35}
-                height={35}
-                src={play}
-            />
-            <img
-                width={35}
-                height={35}
-                src={pause}
-            />
-            <img
-                width={35}
-                height={35}
-                src={next}
-            />
+            <Pause size={35} color="red" />
+            <Play size={35} color="red" />
         </div>
     )
 }
