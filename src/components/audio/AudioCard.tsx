cf4a198444afee80cade87520377447d9015bc97
kapehh <mail@kapehh.net>
2024-07-29 03:39:26 +0300

    poc

--- src/components/audio/AudioCard.tsx
+++ src/components/audio/AudioCard.tsx
@@ -1,12 +1,66 @@
+import { useRef, useState, useTransition } from 'react'
 import Pause from '../images/pause.tsx'
 import Play from '../images/play.tsx'
 import './AudioCard.css'
 
-export default function() {
+export default function () {
+    const [playing, setPlaying] = useState(false)
+    const [position, setPosition] = useState(0)
+    const [duration, setDuration] = useState(0)
+    const audioRef = useRef<HTMLAudioElement>(null)
+
+    const middleButton = playing
+        ? <Pause
+            size={35}
+            color="white"
+            onClick={() => {
+                audioRef.current!.pause()
+            }}
+        />
+        : <Play
+            size={35}
+            color="white"
+            onClick={() => {
+                audioRef.current!.play()
+            }}
+        />
+
     return (
-        <div>
-            <Pause size={35} color="red" />
-            <Play size={35} color="red" />
+        <div className="audio-container">
+            <span className="audio-button">
+                {middleButton}
+            </span>
+            <input
+                type="range"
+                min={0} max={duration}
+                value={position}
+                onChange={e => {
+                    audioRef.current!.currentTime = e.currentTarget.valueAsNumber
+                }}
+            />
+            <audio
+                ref={audioRef}
+                onTimeUpdate={e => {
+                    const audio = e.currentTarget
+
+                    setPosition(audio.currentTime)
+                }}
+                onPause={e => {
+                    const audio = e.currentTarget
+
+                    setPlaying(false)
+                }}
+                onPlay={e => {
+                    const audio = e.currentTarget
+
+                    setPlaying(true)
+                    setDuration(audio.duration)
+                }}
+                onSeeked={e => {
+                    const audio = e.currentTarget
+                }}
+                src="http://music.kapehh.net:8787/Rammstein/Auslander.flac"
+            />
         </div>
     )
 }
