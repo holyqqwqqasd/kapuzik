218bf7d9d42f872db1cf8eaea67cbedaa9a8cf35
kapehh <mail@kapehh.net>
2024-09-18 01:20:45 +0300

    fix gen script

--- generate_config.py
+++ generate_config.py
@@ -1,49 +1,41 @@
-import sys
+import os
+import json
 
-# find . -type f -printf "%h|%p\n"
-f = open(sys.argv[1], "r")
+BASE_URL = "http://music.kapehh.net:8787/"
+
+path_to_music = "/home/karen/Downloads/test_for_script/"
 
 uniq_id = 0
-
 def get_new_id():
     global uniq_id
     uniq_id += 1
     return uniq_id
 
-playlists = {}
+def gen_track(file_name):
+    splits = file_name.split(".")[0].split("-")
 
-for line in f.readlines():
-    raw_playlist, raw_song = line.split("|")
-    playlist = raw_playlist[2:]
-    song = raw_song[2:].strip()
-
-    if playlist in playlists:
-        playlists[playlist].append(
-            {
-                "id": get_new_id(),
-                "name": song,
-                "url": "http://music.kapehh.net:8787/" + song
-            })
-    else:
-        playlists[playlist] = [
-            {
-                "id": get_new_id(),
-                "name": song,
-                "url": "http://music.kapehh.net:8787/" + song
-            }
-        ]
+    return {
+        "id": get_new_id(),
+        "url": (relative_path + "/" + file_name),
+        "name": splits[1].strip() if len(splits) > 1 else splits[0].strip(),
+        "artist": splits[0].strip() if len(splits) > 1 else None
+    }
 
 result = []
 
-for p in playlists:
-    result.append(
-        {
-            "id": get_new_id(),
-            "name": p,
-            "tracks": playlists[p]
-        }
-    )
+for root, dirs, files in os.walk(path_to_music):
+    if files:
+        cover = next((x for x in files if x.endswith((".png", ".jpg", ".jpeg"))), None)
+        tracks = [x for x in files if x.endswith((".flac", ".mp3"))]
+        if tracks:
+            relative_path = root.removeprefix(path_to_music)
+            result.append(
+                {
+                    "id": get_new_id(),
+                    "name": relative_path,
+                    "cover": cover,
+                    "tracks": [gen_track(x) for x in tracks]
+                }
+            )
 
-print("const data: Playlist[] =")
-print(result)
-print("export default data")
+print(json.dumps(result, indent=4))
