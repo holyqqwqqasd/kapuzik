7c6ddff04cd732201293a379102efa9ec821d0ca
kapehh <mail@kapehh.net>
2024-09-20 01:19:21 +0300

    fix loader

--- src/components/config/ConfigLoader.tsx
+++ src/components/config/ConfigLoader.tsx
@@ -1,16 +1,49 @@
-import { useRef } from "react"
+import { useRef, useState } from "react"
 
 interface State {
     onSave: (jsonConfig: string) => void
 }
 
-export default function({ onSave }: State) {
+type LoadType = "json" | "file"
+
+export default function ({ onSave }: State) {
+    const [type, setType] = useState<LoadType>("file")
     const refText = useRef<HTMLTextAreaElement>(null)
+    const refFile = useRef<HTMLInputElement>(null)
 
     return (
         <>
-            <textarea ref={refText}></textarea>
-            <button onClick={() => onSave(refText.current!.value)}>Save</button>
+            <button onClick={() => setType("json")}>JSON</button>
+            <button onClick={() => setType("file")}>FILE</button>
+            <hr />
+            {type == "json"
+                ? <div>
+                    <textarea ref={refText}></textarea>
+                    <button onClick={() => onSave(refText.current!.value)}>Save</button>
+                </div>
+                : null}
+            {type == "file"
+                ? <div>
+                    <input type="file" ref={refFile} />
+                    <button onClick={() => {
+                        let files = refFile.current!.files
+                        if (!files) {
+                            return
+                        }
+
+                        let reader = new FileReader()
+                        reader.onload = function (event) {
+                            let json = event.target!.result
+                            if (!json || typeof (json) != "string") {
+                                return
+                            }
+
+                            onSave(json)
+                        };
+                        reader.readAsText(files[0])
+                    }}>Load</button>
+                </div>
+                : null}
         </>
     )
 }
